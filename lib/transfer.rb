475e9372ae647c9670110476b4dc10a486264edf
Shaina C <scailles23@gmail.com>
2021-01-06 23:09:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ class Transfer
   
   
   def execute_transaction
-    if @sender.balance > @amount && @status = "complete"
+    if @sender.balance > @amount && @status = "pending"
       @status = "complete"
       @sender.balance -= @amount
       @receiver.balance += @amount
