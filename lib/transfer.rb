e06ad4ef49b741bfe28b60d7b178aa07d6c4dfaa
Shaina C <scailles23@gmail.com>
2021-01-06 23:22:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
   
 
   def execute_transaction
-    if !(valid?)
+    if !(sender.valid?)
       puts "Transaction rejected. Please check your account balance."
   
     elsif @status == "complete"
