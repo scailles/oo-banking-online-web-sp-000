773315c1832640470d455c849faed5770d35866a
Shaina C <scailles23@gmail.com>
2021-01-06 23:16:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,22 +21,20 @@ class Transfer
   end
   
   
+
   def execute_transaction
-    
-    if sender.balance > amount
-      status = "rejected"
-      puts "could not be completed."
-    
-    elsif @status == "completed"
-      puts "Transaciton was already completed."
-      
-    else 
-     @sender.deposit( @amount * -1 ) 
+    if @sender.balance < @amount
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+
+    elsif @status == "complete"
+      puts "Transaction was already excuted"
+    else
+      @sender.deposit( @amount * -1 ) 
       @receiver.deposit( @amount )
       @status = "complete"
     end
-    end
-  
+  end
   
   
   
