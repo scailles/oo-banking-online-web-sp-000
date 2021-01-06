adb3d321efbcfade946655d090708f18bc957e33
Shaina C <scailles23@gmail.com>
2021-01-06 22:48:44 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,9 @@
 class BankAccount
   
-  attr_accessor :name
+  attr_accessor :name, :balance, :status
 
 
-  def initialize
+  def initialize(name)
     @name
     @balance = 1000
     @status = "open"
